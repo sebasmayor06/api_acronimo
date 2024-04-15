bd91a775362c866bf7e456ac4de2e67340b18971
sebasmayor06 <sebda06@gmail.com>
2024-04-15 01:15:54 -0500

    DATABASE_URL2

--- src/models/acronimos.js
+++ src/models/acronimos.js
@@ -2,7 +2,7 @@ const {DataTypes} =require("sequelize");
 
 
 module.exports = (database) =>{
-    database.define('busquedas', {
+    database.define('busquedas2', {
 
         id:{
             type: DataTypes.INTEGER, 
