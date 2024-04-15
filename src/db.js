551f85d0e7ebb6683762eb2afa0e09b7570dc10a
sebasmayor06 <sebda06@gmail.com>
2024-04-15 00:31:59 -0500

    DATABASE_URL2

--- src/db.js
+++ src/db.js
@@ -1,16 +1,10 @@
 require('dotenv').config();
 const acronimosModel = require ('./models/acronimos')
-const {
-    DATABASE_URL
-} = process.env
+const { DATABASE_URL } = process.env;
+const { Sequelize } = require('sequelize');
 
-
-const {Sequelize} = require('sequelize');
-
-
-const database = new Sequelize(`${DATABASE_URL}`,{logging:false});
+const database = new Sequelize(DATABASE_URL, { logging: false });
 
 acronimosModel(database)
 
-
-module.exports = {database, ...database.models}
+module.exports = { database, ...database.models };
